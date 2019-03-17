2b09101b84def425d61e602d05fd66c8314092ec
mkexcelmelb <47077486+mkexcelmelb@users.noreply.github.com>
2019-03-17 11:34:22 +1100

    First commit for solving and caching matrices
    As adapted from the cacheMean and makeVector examples

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Combined these functions will cache and perform solving for invers on a matrix
 
-## Write a short comment describing this function
-
+## This functions keeps a cache of the matrix and its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## gets the cached solved inverse of the matrix if it exists, otherwises solves and saves
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+
